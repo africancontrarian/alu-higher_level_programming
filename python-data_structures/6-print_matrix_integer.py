edc3ea04ef64deee0a15dfc87cb5b8a1c990bb60
africancontrarian <t.kambare@alustudent.com>
2026-07-08 21:41:12 +0200

    6. Lists of lists = Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join("{}".format(element) for element in row))
+        print(" ".join("{:d}".format(element) for element in row))
