a866d1e236229905787bb0c91613d8def975f52b
africancontrarian <t.kambare@alustudent.com>
2026-07-10 18:40:47 +0200

    13. Weighted average!

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,4 +2,4 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    return sum(score * weight for score, weight in my_list) / sum(weight for weight in my_list)
+    return sum(score * weight for score, weight in my_list) / sum(weight for score, weight in my_list)
