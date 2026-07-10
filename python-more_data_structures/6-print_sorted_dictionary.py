0663a9c852af3e99028044c8ce58e418144d3371
africancontrarian <t.kambare@alustudent.com>
2026-07-10 10:22:29 +0200

    6. Print sorted dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 
-def print_sorted_dictionary():
+def print_sorted_dictionary(a_dictionary):
     for key in sorted():
         print("{:d}: {:d}".format(key, a_dictionary[key]))
