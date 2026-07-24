6fca17c088c32cf9aad60c285e8fc65c3c5aa5f8
Tapiwanashe Kambare [TK] <t.kambare@alustudent.com>
2026-07-24 02:21:24 +0200

    Update 100-move_to_utf8.sql

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,13 @@
+-- Convert the hbtn_0c_0 database to utf8mb4
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
+-- Convert first_table to utf8mb4
 ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
+
+-- Change only the column collation
+ALTER TABLE hbtn_0c_0.first_table
+MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
